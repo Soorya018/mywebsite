188ece9cc388a14b07bbc0c573380a49aaa43689
Arun Soorya K <arunsoorya108@gmail.com>
2022-02-13 20:14:18 +0530

    Updates

---
+++ precache-manifest.6770e2490501a5b7d0731676911d1506.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bab1d948dd8ca369acb9915176ce91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4b2b16371e15b1660b0",
+    "url": "/static/css/main.e0b78bfd.chunk.css"
+  },
+  {
+    "revision": "e67ed16403b8da2ba367",
+    "url": "/static/js/2.e53e0ff9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.e53e0ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b2b16371e15b1660b0",
+    "url": "/static/js/main.67e8b77f.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1d8b562cf1ef53c667d7a9aacd92d034",
+    "url": "/static/media/arunsoorya.1d8b562c.jpg"
+  },
+  {
+    "revision": "c8b5f9dad975c16e6657a7bdc5b5ea94",
+    "url": "/static/media/resume.c8b5f9da.pdf"
+  },
+  {
+    "revision": "1e78182f2870ef5f7277b10047c556e1",
+    "url": "/static/media/zoho-logo.1e78182f.png"
+  }
+]);
