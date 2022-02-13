c1d8f1c1cd2628ca9b3dcf635d8e18f1a76a57ad
Arun Soorya K <arunsoorya108@gmail.com>
2022-02-13 20:13:05 +0530

    deploy my app

---
+++ build/precache-manifest.1b0e80b00077db5990961086e1d59d61.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee90164084fcd3939016fb44d22878e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c90980ed4a645ae0eda5",
+    "url": "/home/static/css/main.e0b78bfd.chunk.css"
+  },
+  {
+    "revision": "e67ed16403b8da2ba367",
+    "url": "/home/static/js/2.e53e0ff9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.e53e0ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90980ed4a645ae0eda5",
+    "url": "/home/static/js/main.2bbd5c94.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "1d8b562cf1ef53c667d7a9aacd92d034",
+    "url": "/home/static/media/arunsoorya.1d8b562c.jpg"
+  },
+  {
+    "revision": "c8b5f9dad975c16e6657a7bdc5b5ea94",
+    "url": "/home/static/media/resume.c8b5f9da.pdf"
+  },
+  {
+    "revision": "1e78182f2870ef5f7277b10047c556e1",
+    "url": "/home/static/media/zoho-logo.1e78182f.png"
+  }
+]);
---
+++ src/editable-stuff/config.js
@@ -0,0 +1,157 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  firstName: "Arun Soorya",
+  middleName: "",
+  lastName: "K",
+  message: " Passionate about changing the world with technology. ",
+  icons: [
+    {
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/arunsooryak/",
+    },
+    {
+      image: "fa-stack-overflow",
+      url: "https://stackoverflow.com/users/12929660/arun-soorya",
+    },
+    {
+      image: "fa-github",
+      url: "https://github.com/Soorya018",
+    },
+    
+    {
+      image: "fa-instagram",
+      url: "https://www.instagram.com/lost___lllusion/",
+    },
+    
+    {
+      image: "fa-twitter",
+      url: "https://twitter.com/Arun__Soorya",
+    },
+     
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+
+const about = {
+  show: true,
+  heading: "About Me",
+  imageLink: require("../editable-stuff/arunsoorya.jpg"),
+  imageSize: 375,
+  message:
+    "My name is Arun Soorya. I’m a graduate in Computer Engineering currently working as Technical Support Engineer at Zoho Corporation. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering.",
+  resume: "https://drive.google.com/file/d/10tJPiycVkxuoNOY0V_ttBOmNaL4UQWeI/view?usp=sharing",
+};
+
+// PROJECTS SECTION
+// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+const repos = {
+  show: true,
+  heading: "Recent Projects",
+  gitHubUsername: "Soorya018", //i.e."johnDoe12Gh"
+  reposLength: 2,
+  specificRepos: [],
+};
+
+// Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    { 
+      img: require("../editable-stuff/arunsoorya.jpg"), 
+      label: "First slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+    { 
+      img: require("../editable-stuff/arunsoorya.jpg"), 
+      label: "Second slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+  ],
+  imageSize: {
+    width:"615",
+    height:"450"
+  }
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  heading: "Skills",
+  hardSkills: [
+    { name: "Python", value: 90 },
+    { name: "PostgreSQL", value: 75 },
+    { name: "MSSQL", value: 85 },
+    { name: "JavaScript", value: 90 },
+    { name: "ReactJS", value: 65 },
+    { name: "HTML/CSS", value: 55 },
+    
+  ],
+  softSkills: [
+    { name: "Goal-Oriented", value: 80 },
+    { name: "Collaboration", value: 90 },
+    { name: "Positivity", value: 75 },
+    { name: "Adaptability", value: 85 },
+    { name: "Problem Solving", value: 75 },
+    { name: "Empathy", value: 90 },
+    { name: "Customer Oriented", value: 70 },
+    { name: "Creativity", value: 90 },
+  ],
+};
+
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "Get In Touch",
+  message:
+    "If you wish to reach out to me, drop an email to",
+  email: "contact@arunsoorya.com"
+};
+
+const experiences = {
+  show: false,
+  heading: "Experiences",
+  data: [
+    {
+      role: 'Technical Support Engineer, Zoho Corporation',// Here Add Company Name
+      companylogo: require('../assets/img/zoho-logo.png'),
+      date: 'January 2021 – Present',
+    },
+    {
+      role: 'Front-End Developer',
+      companylogo: require('../assets/img/boeing.png'),
+      date: 'May 2017 – May 2018',
+    },
+  ]
+}
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
